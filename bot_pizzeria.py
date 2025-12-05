48fb7f2715c45feb8d2454747d462c5cfca0f952
Netchirp <jinxynet@duck.com>
2025-12-05 18:27:18 +0100

    Añadir funcion añadir_ingrediente, salir, y refactorizado el codigo para que se vea mejor

--- bot_pizzeria.py
+++ bot_pizzeria.py
@@ -3,29 +3,38 @@ COLOR_AZUL: str = "\033[34m"
 COLOR_AMARILLO: str = "\033[33m"
 COLOR_VERDE: str = "\033[32m"
 COLOR_ROJO: str = "\033[31m"
-RESETEO_COLOR:str = "\033[0m"
+RESETEO_COLOR: str = "\033[0m"
 
 # --- DATOS INICIALES ---
 menu = ["Margarita", "Cuatro Quesos", "Pepperoni", "Hawaiana"]
 stock_ingredientes = ["queso", "tomate", "cebolla", "aceitunas", "champiñones", "jamon"]
 
 carrito_pizzas = []
-ingredientes_solicitados = []  # Aquí entra to do lo que escriba el usuario. Tanto correcto como incorrecto.
+ingredientes_solicitados = (
+    []
+)  # Aquí entra to do lo que escriba el usuario. Tanto correcto como incorrecto.
 
 print(f"{COLOR_AMARILLO}¡Bienvenido a Pizzería Pythoni!{RESETEO_COLOR}")
-print(f"{COLOR_VERDE}Comandos disponibles: 'pedir', 'extra', 'ver', 'pagar', 'salir'{RESETEO_COLOR}")
+print(
+    f"{COLOR_VERDE}Comandos disponibles: 'pedir', 'extra', 'ver', 'pagar', 'salir'{RESETEO_COLOR}"
+)
 
 # --- INICIO DE PROGRAMA ---
+
 
 def mostrar_menu():
     print("--- MENU ---")
     for pizza in menu:
-        print(pizza)
+        print(f"* {pizza}")
     print("------------")
 
+
 def mostrar_stock_ingredientes():
+    print("--- EXTRA ---")
     for ingrediente in stock_ingredientes:
-        print(ingrediente)
+        print(f"* {ingrediente}")
+    print("------------")
+
 
 def anadir_pizza(pizza):
     for i in range(len(menu)):
@@ -35,6 +44,10 @@ def anadir_pizza(pizza):
             break
 
 
+def anadir_ingrediente(ingrediente):
+    ingredientes_solicitados.append(ingrediente)
+
+
 continuar: bool = True
 while continuar:
     print("----------")
@@ -42,16 +55,23 @@ while continuar:
     match comando.lower():
         case "pedir":
             mostrar_menu()
-            seleccion_pizza: str = input("Cual de estas pizzas desea pedir?\nSelección: ")
+            seleccion_pizza: str = input(
+                "Cual de estas pizzas desea pedir?\nSelección: "
+            )
             anadir_pizza(seleccion_pizza.title())
         case "extra":
             mostrar_stock_ingredientes()
-            print("Que ingrediente deseas extra?\n -> ")
+            ingrediente_solicitado: str = input("Que ingrediente deseas extra?\n -> ")
+            anadir_ingrediente(ingrediente_solicitado)
+            print(
+                f"{COLOR_VERDE}Ingrediente {ingrediente_solicitado} añadido al carrito{RESETEO_COLOR}"
+            )
         case "ver":
             pass
         case "pagar":
             pass
         case "salir":
-            pass
+            print(f"{COLOR_AMARILLO}Mamma Mia{RESETEO_COLOR}")
+            break
         case _:
             print(f"{COLOR_ROJO} Comando desconocido {RESETEO_COLOR}")
